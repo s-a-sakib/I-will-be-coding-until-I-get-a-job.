71ffdfa40e09ac08372fa1a7c4730947537013cb
sakib <shahrearalsakib16@gmail.com>
2026-03-05 00:02:36 +0600

    Day 2 second

---
+++ 2191A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n, a;
+    cin >> n;
+    map < int , int > m;
+    for(int i = 0; i < n; i++){
+        cin >> a;
+        m[a] = (i % 2);
+    }
+    for(int i = 1; i < n; i++){
+        if(m[i] == m[i + 1]){
+            cout << "NO" << endl;
+            return;
+        }
+    }
+    cout << "YES" << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
